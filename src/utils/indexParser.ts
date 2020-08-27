caec7b0168d3ad92d3d32086b56bb2a5ded61cd2
Waterdev <valentin.john@gmx.net>
2020-08-27 18:40:48 +0200

    fixed my dumbassery xD

--- src/utils/indexParser.ts
+++ src/utils/indexParser.ts
@@ -1,6 +1,5 @@
 // most names in the api can be casted to an index quite easily, which allows for less frustrating search results
 
 export default function parseIndex(name: string) {
-	Regex rgx = new Regex("[^a-zA-Z0-9]");
-	return name.toLowerCase().replace(/\s/g, "-").replace(rgx, "");
+	return name.toLowerCase().replace(/\s/g, "-").replace("[^a-zA-Z0-9]", "");
 }
