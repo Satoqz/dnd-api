2ac93cc0fd961f672e4f75e4f7f5e1ae10db2aa3
UnrealValentin <30842467+UnrealValentin@users.noreply.github.com>
2020-08-27 18:34:03 +0200

    Optimized
    Added an optimisation

--- src/utils/indexParser.ts
+++ src/utils/indexParser.ts
@@ -1,5 +1,6 @@
 // most names in the api can be casted to an index quite easily, which allows for less frustrating search results
 
 export default function parseIndex(name: string) {
-	return name.toLowerCase().replace(/\s/g, "-").replace(/'/gm, "");
+	Regex rgx = new Regex("[^a-zA-Z0-9]");
+	return name.toLowerCase().replace(/\s/g, "-").replace(rgx, "");
 }
